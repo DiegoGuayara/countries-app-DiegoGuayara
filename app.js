293d5b01fb3a0201b348166cce3566fab5c991c4
DiegoGuayara <diegofguayara9707@gmail.com>
2025-04-30 08:16:42 -0500

    Avance 2. Organicé las tarjetas

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 import { countries } from "./countries.js";
 
-const cont1 = document.querySelector(".country-card");
+const cont1 = document.querySelector(".countries");
 
 countries.map(({ country, capital, population }) => {
   const div = document.createElement("div");
-  div.className = "country";
+  div.className = "country-card";
   div.innerHTML = `<h2>${country}</h2><p>Capital: ${capital}</p><p>Population: ${population}</p><br/> <hr/>`;
 
-  cont1.appendChild(div)
+  cont1.appendChild(div);
 });
-
